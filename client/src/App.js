31a9bffec4b93212ae0a1fb4be58859a1527b112
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-12-23 17:06:20 -0800

    made minimum progress on client side today

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Provider } from "redux-thunk";
+import { Container } from "reactstrap";
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Container>
+          Hello
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
