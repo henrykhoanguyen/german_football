d6e0a9742e9ce252978b84b6024c1f4df74405e0
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-01-15 12:35:18 -0800

    pagination in matches table is working

--- german-football-client/src/app/matches-table/matches-table.component.ts
+++ german-football-client/src/app/matches-table/matches-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog, MatPaginator } from '@angular/material';
+import { MatDialog, MatPaginator, PageEvent } from '@angular/material';
 // Services
 import { FootballService } from '../services/football.service';
 // Components
@@ -16,29 +16,42 @@ export class MatchesTableComponent implements OnInit {
   private matches: [] = [];
   private length: number;
 
-  private static limit = 9;
+  private pagedMatches = []; // only has 9 matches to be displayed on each page
+  private pageIndex = 0; // pageIndex by default is 0
+  private pageSize = 9;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   private seasons = [];
 
-  constructor(private footballService: FootballService, public dialog: MatDialog) {}
+  constructor(
+    private footballService: FootballService,
+    public dialog: MatDialog
+  ) {}
   // , private seasonsButton: SeasonsButtonComponent
   ngOnInit() {
     this.getAllSeasons();
-    // this.seasons = this.seasonsButton.getSeasons();
-    // this.inputSeason = this.seasonsButton.getSeason();
   }
 
+  // Get all matches in a given season
   getMatches(season): void {
     this.footballService
       .getMatchesTable('?Season=' + String(season))
       .subscribe(res => {
         // console.log(res);
-        this.matches = Object(res).data;
-        this.length = Object(res).count;
+        this.matches = Object(res).data; // get matches
+        this.length = Object(res).count; // get number of matches
+        // set pageIndex to current pageIndex that user is on
+        this.OnPageChange({
+          pageIndex: this.pageIndex,
+          pageSize: this.pageSize,
+          length: this.length
+        });
+        // console.log(this.pagedMatches);
       });
+
   }
 
+  // Get all seasons
   getAllSeasons(): void {
     this.footballService.getSeasons().subscribe(res => {
       this.seasons = [...Object(res).data];
@@ -48,14 +61,36 @@ export class MatchesTableComponent implements OnInit {
     // console.log(this.seasons);
   }
 
+  // Set season for season selection button
   setSeason(season): void {
     this.inputSeason = season;
     // console.log(season);
     this.getMatches(season);
   }
 
-  openDialog(match){
+  // Open a match dialog/modal
+  // that has match's info in it.
+  openDialog(match) {
     console.log(match);
     this.dialog.open(MatchDetailsDialogComponent, { data: match });
+  }
+
+  // Pagination
+  OnPageChange(event: PageEvent) {
+    // console.log(event);
+    // set pageIndex to current pageIndex that user is on
+    this.pageIndex = event.pageIndex;
+    // console.log(this.pageIndex);
+
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+
+    if (endIndex > this.length) {
+      endIndex = this.length;
+    }
+    // console.log(startIndex, endIndex);
+
+    this.pagedMatches = this.matches.slice(startIndex, endIndex);
+    // console.log(this.pagedMatches);
   }
 }
