5b0226f3c2727e3d0e4fd0888821045a3bd04163
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-01-25 17:39:02 -0800

    fix server problem with direction to client file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const errorHandler = require("./middleware/errors");
 const cors = require('cors');
+const path = require('path');
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -31,10 +32,10 @@ app.use(errorHandler);
 // Serve static assests if in production
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('german-football-client/dist'));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'german-football-client', 'dist', 'index.html'));
   });
 }
 
